475ae419af9566c293a728c22b724301e613634b
IMP Studio <impstudio@arrasyid.local>
2019-07-30 23:45:42 +0700

    add tambah activity

--- app/src/main/java/com/example/myapplication/Dashboard.kt
+++ app/src/main/java/com/example/myapplication/Dashboard.kt
@@ -1,12 +1,19 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class Dashboard : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        btnTambah.setOnClickListener{
+            val intent = Intent(this, Tambah::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/Tambah.kt
+++ app/src/main/java/com/example/myapplication/Tambah.kt
@@ -2,11 +2,36 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_tambah.*
+import android.widget.DatePicker
+import android.app.DatePickerDialog
+import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
+import java.util.*
+
 
 class Tambah : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tambah)
+
+        btnCalendar.setOnClickListener{
+            val newCalendar = Calendar.getInstance()
+
+            val datePickerDialog = DatePickerDialog(this@Tambah,
+                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                    val newDate = Calendar.getInstance()
+                    newDate.set(year, monthOfYear, dayOfMonth)
+                    val dateFormatter = SimpleDateFormat("dd-MM-yyyy", Locale.US)
+                    inputTanggalLahir.setText(dateFormatter.format(newDate.getTime()))
+                },
+                newCalendar.get(Calendar.YEAR),
+                newCalendar.get(Calendar.MONTH),
+                newCalendar.get(Calendar.DAY_OF_MONTH)
+            )
+
+            datePickerDialog.show()
+        }
     }
 }
